251648f67118a81efabff9fe8fd37f685159c8d6
Filippo Gangi Dino <filippo.gangidino@gmail.com>
2021-10-20 11:14:30 +0200

    Text component

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,19 @@
 <script>
-	import data from './_data.json';
-	import "swiper/css/navigation";
-  import 'swiper/css';
+	import data from './_data.json'
+	import "swiper/css/navigation"
+  import 'swiper/css'
 
-	import BgCards from './components/BgCards.svelte';
-	import BlockWithImage from './components/BlockWithImage.svelte';
-	import Bubbles from './components/Bubbles.svelte';
-	import Cards from './components/Cards.svelte';
-	import Carousel from './components/Carousel.svelte';
-	import Footer from './components/Footer.svelte';
-	import Header from './components/Header.svelte';
-	import Hero from './components/Hero.svelte';
-	import Spacer from './components/Spacer.svelte';
-	import Title from './components/Title.svelte';
+	import BgCards from './components/BgCards.svelte'
+	import BlockWithImage from './components/BlockWithImage.svelte'
+	import Bubbles from './components/Bubbles.svelte'
+	import Cards from './components/Cards.svelte'
+	import Carousel from './components/Carousel.svelte'
+	import Footer from './components/Footer.svelte'
+	import Header from './components/Header.svelte'
+	import Hero from './components/Hero.svelte'
+	import Spacer from './components/Spacer.svelte'
+	import Text from './components/Text.svelte'
+	import Title from './components/Title.svelte'
 
 	let loadedComponents = {
 		"BgCards": BgCards,
@@ -22,6 +23,7 @@
 		"Carousel": Carousel,
 		"Hero": Hero,
 		"Spacer": Spacer,
+		"Text": Text,
 		"Title": Title
 	}
 	// Hardcoded before routing
