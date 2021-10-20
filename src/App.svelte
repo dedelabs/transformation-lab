61d02a9f1ff067768bcc288b8aedbebacbd07f3b
Filippo Gangi Dino <filippo.gangidino@gmail.com>
2021-10-20 11:06:38 +0200

    New Title component

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,17 @@
 	import data from './_data.json';
 	import "swiper/css/navigation";
   import 'swiper/css';
+
 	import BgCards from './components/BgCards.svelte';
 	import BlockWithImage from './components/BlockWithImage.svelte';
 	import Bubbles from './components/Bubbles.svelte';
 	import Cards from './components/Cards.svelte';
 	import Carousel from './components/Carousel.svelte';
+	import Footer from './components/Footer.svelte';
+	import Header from './components/Header.svelte';
 	import Hero from './components/Hero.svelte';
 	import Spacer from './components/Spacer.svelte';
-	import Header from './components/Header.svelte';
-	import Footer from './components/Footer.svelte';
+	import Title from './components/Title.svelte';
 
 	let loadedComponents = {
 		"BgCards": BgCards,
@@ -19,7 +21,8 @@
 		"Cards": Cards,
 		"Carousel": Carousel,
 		"Hero": Hero,
-		"Spacer": Spacer
+		"Spacer": Spacer,
+		"Title": Title
 	}
 	// Hardcoded before routing
 	$: components = data.pages.filter((p) => p.name == 'Home')[0].components;
