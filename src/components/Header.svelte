75188a6d7ffd30547a2b022e1081a466911a44c5
andreareginato <andrea.reginato@gmail.com>
2021-10-22 12:06:15 +0200

    refine header menu items

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -20,18 +20,18 @@
     <div class="header__menu--mobile" transition:fly="{{ y: 200, duration: 500 }}" >
       <ul class="header__menu">
         <li class="header__menu__item"><span data-smoothscroll="#Solution">Solution</span></li>
-        <li class="header__menu__item"><span data-smoothscroll="#What">Our Value</span></li>
-        <li class="header__menu__item"><span data-smoothscroll="#Micromaster">Services</span></li>
-        <li class="header__menu__item"><span data-smoothscroll="#Who">To who</span></li>
+        <li class="header__menu__item"><span data-smoothscroll="#What">Services</span></li>
+        <li class="header__menu__item"><span data-smoothscroll="#Micromaster">Micromasters</span></li>
+        <li class="header__menu__item"><span data-smoothscroll="#Who">Target</span></li>
       </ul>
       <!-- <a class="header__button header__button--mobile" href="##">Open call</a> -->
     </div>
   {/if}
   <ul class="header__menu header__menu--desk">
     <li class="header__menu__item"><span data-smoothscroll="#Solution">Solution</span></li>
-    <li class="header__menu__item"><span data-smoothscroll="#What">Our Value</span></li>
-    <li class="header__menu__item"><span data-smoothscroll="#Micromaster">Services</span></li>
-    <li class="header__menu__item"><span data-smoothscroll="#Who">To who</span></li>
+    <li class="header__menu__item"><span data-smoothscroll="#What">Services</span></li>
+    <li class="header__menu__item"><span data-smoothscroll="#Micromaster">Micromasters</span></li>
+    <li class="header__menu__item"><span data-smoothscroll="#Who">Target</span></li>
   </ul>
   <!-- <a class="header__button header__button--desk" href="##">Open call</a> -->
 </header>
